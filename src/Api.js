083325b9c04980419cfe99814d7beabbf8fe12ee
Sergey <heitei257@gmail.com>
2019-10-03 17:56:19 +0400

    fixes

--- src/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-let api = axios.create({
-  headers: {
-    'Client-ID': '57sp3gfmuynhhntab2znwu88s8cjxi'
-  }
-})
-
-export default api
--- src/store/state.js
+++
@@ -1,8 +0,0 @@
-export default {
-  games: null,
-  gameStreams: null,
-  gameTopStreams: null,
-  user: null,
-  searchChannel: null,
-  searchGame: null
-}
